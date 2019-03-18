fd10dd63a028127bcc567d06ade20b7785c19234
Ryan Neudorf <ryan.neudorf@gmail.com>
2019-03-18 00:33:50 -0500

    migration update

--- database/migrations/2019_03_17_194356_create_feed_items_table.php
+++ database/migrations/2019_03_17_194356_create_feed_items_table.php
@@ -15,7 +15,7 @@ class CreateFeedItemsTable extends Migration
     {
         Schema::create('feed_items', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigInteger('feeds_id')->nullable();
+            $table->bigInteger('feeds_id')->nullable()->default(null);
             $table->string('title')->nullable();
             $table->string('content')->nullable();
             $table->string('permalink')->nullable();
